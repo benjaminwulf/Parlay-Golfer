2cb2391db3c4e422153cefc429e81ab719e86e5b
Benjamin W Wulf <benjaminwulf@gmail.com>
2018-09-06 17:56:45 -0600

    added schema

--- app/models/schema.sql
+++ app/models/schema.sql
@@ -1,5 +1,20 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS parlay_golfer_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+CREATE DATABASE parlay_golfer_db;
+USE parlay_golfer_db;
+
+CREATE TABLE teams
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+USE parlay_golfer_db;
+INSERT INTO teams (name) VALUES ('Team Benji');
+INSERT INTO teams (name) VALUES ('Team Ryan');
+INSERT INTO teams (name) VALUES ('Team Brian');
+INSERT INTO teams (name) VALUES ('Team Murad');
+
+use Parlay_golfer_db;
+SELECT * FROM teams;
