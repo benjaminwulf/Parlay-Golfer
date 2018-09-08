f2d09ffffb3c4f5a4c4755ab8ce9bded657864f5
Brian Padilla <bpadil123@gmail.com>
2018-09-08 13:05:04 -0600

    routes/ejs files

--- app/data/golfCourses/course2127.js
+++ app/data/golfCourses/course2127.js
@@ -5,44 +5,43 @@ var golfCourses = [{
     },
     {
         name: "hole2",
-        photo: "https://www.therichest.com/world-entertainment/15-reasons-why-jim-halpert-is-a-massive-douchebag/",
+        photo: "../images/hole2.png",
        
     },
     {
         name: "hole3",
-        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjZsPes1_DcAhUBlawKHfWPAg0QjRx6BAgBEAU&url=https%3A%2F%2Falchetron.com%2FPam-Beesly&psig=AOvVaw0eEGskDpg6Ze_k1lvysvrS&ust=1534478300002721",
+        photo: "../images/hole3.png",
        
     },
     {
         name: "hole4",
-        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwih3IKI1_DcAhVQ-6wKHY5kCKcQjRx6BAgBEAU&url=https%3A%2F%2Fwww.dundermifflin.us%2Fteam%2Fdwight-schrute&psig=AOvVaw1PZAEiLZoSBm55gbOyCVcY&ust=1534478224773849",
+        photo: "../images/hole4.png",
         
     },
     {
         name: "hole5",
-        photo: "https://i.pinimg.com/originals/39/63/33/3963339ad1f8cd20ddffb5edb10497ca.jpg",
+        photo: "../images/hole5.png",
         
     },
     {
         name: "hole6",
-        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj6noD71_DcAhUHbK0KHRAkDDYQjRx6BAgBEAU&url=https%3A%2F%2Ftwitter.com%2Ftoflenderson&psig=AOvVaw0K_8SQfGAYzlsww-JZ-ShA&ust=1534478464516065",
+        photo: "../images/hole6.png",
         
     },
     {
         name: "hole7",
-        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiJ1eOR2PDcAhUIQq0KHREnB28QjRx6BAgBEAU&url=https%3A%2F%2Fwww.pinterest.com%2FOfficeCharacter%2Fmeredith-palmer%2F&psig=AOvVaw3fB0pwxtJ9PFnbfesGGvW9&ust=1534478510347654",
-        
+        photo: "../images/hole7.png",        
     },
     {
         name: "hole8",
-        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwizxaCm2PDcAhUEiqwKHWtxCG4QjRx6BAgBEAU&url=https%3A%2F%2Ftwitter.com%2Fjimpamfans&psig=AOvVaw1daSlyqARhr2UZTKGnJ1bd&ust=1534478559270434",
+        photo: "../images/hole8.png",        
         
     },
    
 
     {
         name: "hole9",
-        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiFgunQ2PDcAhVL-qwKHX4fBG4QjRx6BAgBEAU&url=https%3A%2F%2Fgiphy.com%2Fgifs%2Fkelly-kapoor-xbHTJehx2x6vK&psig=AOvVaw1O-FKMRIs3WWy6LzZ4P-Xi&ust=1534478625041694",
+        photo: "../images/hole9.png",        
         
     },
 
