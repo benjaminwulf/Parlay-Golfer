19f71bb28be5d15b2da457a6ccca9155f7aaa88f
Benjamin W Wulf <benjaminwulf@gmail.com>
2018-09-18 19:03:36 -0600

    updated callback

--- app/config/auth.js
+++ app/config/auth.js
@@ -3,5 +3,5 @@
 // expose our config directly to our application using module.exports
 // console.log("PROCESS ENV.....", process.env.NODE_ENV);
 
-// module.exports = require('./environment/development.js');
-module.exports = require('./environment/production.js');
+module.exports = require('./environment/development.js');
+// module.exports = require('./environment/production.js');
