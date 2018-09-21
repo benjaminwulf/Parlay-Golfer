62b4802754ccfee1120e24fab8bdb6d71fb8b51a
Benjamin W Wulf <benjaminwulf@gmail.com>
2018-09-20 18:07:35 -0600

    || port

--- app/config/auth.js
+++ app/config/auth.js
@@ -2,5 +2,6 @@
 
 // expose our config directly to our application using module.exports
 
-console.log("PROCESS ENV.....", process.env.NODE_ENV);
+console.log("PROCESS ENV.....", process.env.NODE_ENV) || port;
 // module.exports = require('./environment/development.js');
+
