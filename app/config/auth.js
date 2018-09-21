24219f36433fdb9b65a0aee7c21536bc2c3efb2d
Benjamin W Wulf <benjaminwulf@gmail.com>
2018-09-20 18:15:58 -0600

    production setting

--- app/config/auth.js
+++ app/config/auth.js
@@ -2,6 +2,6 @@
 
 // expose our config directly to our application using module.exports
 
-console.log("PROCESS ENV.....", process.env.NODE_ENV) || port;
+// console.log("PROCESS ENV.....", process.env.NODE_ENV);
 // module.exports = require('./environment/development.js');
-
+module.exports = require('./environment/production.js');
